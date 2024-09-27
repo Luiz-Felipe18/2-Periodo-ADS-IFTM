0d8086bdd954cf9d0ca7b20e5cb4946757ca2351
Luiz Felipe <luizfelipetheodoro44@gmail.com>
2024-09-26 21:32:17 -0300

    Add files via upload

---
+++ Atividade 2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<math.h>
+#define PI 3.14159
+ int main() {
+
+   double area, raio;
+
+   scanf ("%lf", &raio);
+
+   area = PI * pow(raio, 2);
+
+   printf ("A=%.4lf", area);
+
+   printf ("\n");
+
+   return 0;
+
+ }

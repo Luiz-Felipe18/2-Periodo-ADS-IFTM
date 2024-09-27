bd4abb6256cf52f99bc1c4f4e87c0ae05dafd3cf
Luiz Felipe <luizfelipetheodoro44@gmail.com>
2024-09-26 21:31:11 -0300

    Add files via upload

---
+++ Atividade 1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+ int main (){
+
+     int a, b, x;
+      scanf ("%d", &a, "\n");
+      scanf ("%d", &b), "\n";
+      x = a + b;
+      printf ("X = %d",x);
+      printf ("\n");
+
+     return 0;
+
+ }
